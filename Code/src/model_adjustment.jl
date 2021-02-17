11dca085d85e68c3c3b40533ba53b71af91afdc0
Wegener <wmd852@wiso.ad.uni-koeln.de>
2021-02-17 18:24:43 +0100

    Changes in models

--- Code/src/model_adjustment.jl
+++ Code/src/model_adjustment.jl
@@ -15,16 +15,22 @@ function calc_optimazation(α,β,scenario_path,P_S_Max,P_W_Max)
     P_Max = P_S_Max + P_W_Max
     d_t_v = 1 
     T = 1
+    N_T_one = 1
     λ_D = scenarios[!,"lambda_D"]
+    λ_A = scenarios[!,"lambda_A"]
     if P_S_Max != 0
         P_S = scenarios[!,"P_t_S"]
+        P_S_τ = scenarios[!,"P_tau_S"]
     else 
         P_S = scenarios[!,"Zeros"]
+        P_S_τ = scenarios[!,"Zeros"]
     end
     if P_W_Max != 0
         P_W = scenarios[!,"P_t_W"]
+        P_w_τ = scenarios[!,"P_tau_W"]
     else 
         P_W = scenarios[!,"Zeros"]
+        P_w_τ = scenarios[!,"Zeros"]
     end
     r_Plus = scenarios[!,"r_plus"]
     r_Minus = scenarios[!,"r_minus"]
@@ -36,17 +42,21 @@ function calc_optimazation(α,β,scenario_path,P_S_Max,P_W_Max)
     set_silent(pool)
     ##Add variables to model
     @variable(pool,P_D[1:Ω,1:T]>=0)
+    @variable(pool,P_A[1:Ω,1:T]>=0)
     @variable(pool,Δ_Plus[1:Ω,1:T]>=0)
     @variable(pool,Δ_Minus[1:Ω,1:T]>=0)
     @variable(pool,η[1:Ω]>=0)
     @variable(pool,ζ)
     @variable(pool,P[1:Ω,1:T]>=0)
+    @variable(pool,Pτ[1:Ω,1:T]>=0)
     @variable(pool,Δ[1:Ω,1:T]>=0)
+    @variable(pool,P_O[1:Ω,1:T]>=0)
     ##Add expressions
     #(1)
     @expression(pool, R[ω in 1:Ω],    
             sum(
             λ_D[ω,t]*P_D[ω,t]*d_t_v+
+            λ_A[ω,t]*P_A[ω,t]*d_t_v +
             λ_D[ω,t]*Δ_Plus[ω,t]*r_Plus[ω,t]-
             λ_D[ω,t]*Δ_Minus[ω,t]*r_Minus[ω,t]
             for t in 1:T)
@@ -58,9 +68,15 @@ function calc_optimazation(α,β,scenario_path,P_S_Max,P_W_Max)
     #(2)
     @constraint(pool, Max_P_D[t in 1:T, ω in 1:Ω], 
         P_D[ω,t] <= P_Max);
-    ##(3)+ (4) Not not necessary
+    #(3)
+    @constraint(pool,Value_P_O[t in 1:T, ω in 1:Ω],P_O[ω,t] == P_D[ω,t]+P_A[ω,t])
+    #(4)
+    @constraint(pool, Rest_Max_P_O[t in 1:T, ω in 1:Ω], 
+    0<=P_O[ω,t] <= P_Max);
     #(5)
     @constraint(pool,Value_P_t_w[t in 1:T, ω in 1:Ω],P[ω,t]==P_S[ω,t]+P_W[ω,t])
+    #(5.1)
+    @constraint(pool,Value_P_τ_w[t in 1:T, ω in 1:Ω],Pτ[ω,t]==P_S_τ[ω,t]+P_w_τ[ω,t])
     #(6)
     @constraint(pool,Value_P_max,P_Max == P_S_Max + P_W_Max)
     #(7)
@@ -84,10 +100,30 @@ function calc_optimazation(α,β,scenario_path,P_S_Max,P_W_Max)
 
         end
     end
-    #(13) Not not necessary
+    #(13)
+    for (t,ω,ωω) in product(1:T,1:Ω,1:Ω)
+        status = false
+        for (t_one) in (1:T) 
+            if λ_D[ωω,t_one]!=λ_D[ω,t_one]
+                status = true
+                break
+            end
+        end
+       
+        for t_two in (1:N_T_one)
+                if Pτ[ωω,t_two] != Pτ[ω,t_two]
+                    status = true
+                    break
+                end
+            end
+        if status == false
+            @constraint(pool,P_A[ω,t] == P_A[ωω,t])
+        end
+    end
     #(14)
     @constraint(pool,Not_neg[ω in 1:Ω],-sum(
         λ_D[ω,t]*P_D[ω,t]*d_t_v+
+        λ_A[ω,t]*P_A[ω,t]*d_t_v +
         λ_D[ω,t]*(Δ_Plus[ω,t]+r_Plus[ω,t]-Δ_Minus[ω,t]+r_Minus[ω,t])
         for t in 1:T)+ζ-η[ω]<=0)
     #(15)
